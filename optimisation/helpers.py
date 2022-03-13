cb2d60bbdce6a2933219d62881b4c72c7cc8e1f2
Gianmarco Ghiandoni <30413455+ghiander@users.noreply.github.com>
2022-03-13 12:32:12 +0000

    added optimisation (#19)
    Co-authored-by: Gian Marco Ghiandoni <gianmarco.ghiandoni@astrazeneca.com>

---
+++ optimisation/helpers.py
@@ -0,0 +1,82 @@
+"""Helpers for the parameter fitting of Jazzy."""
+# optimisation/helpers.py
+import _pickle as cpickle
+import bz2
+import os
+
+import config
+import optuna
+from optuna.samplers import TPESampler
+
+
+def load_data_configuration(study_filename):
+    """Abstraction for input and output loading and configuration setting.
+
+    The method only accepts the name of the output study file, whilst
+    the rest of the parameters must be defined in a config file.
+    """
+    # load the input
+    data_path = os.path.abspath(os.path.join(os.getcwd(), "..", config.DATA_PATH))
+    optuna_path = os.path.join(data_path, config.OPTUNA_DIRNAME)
+    data_filepath = os.path.join(optuna_path, config.PRECALCULATED_DATA_FILENAME)
+    data = bz2.BZ2File(data_filepath, "rb")
+    input_data = cpickle.load(data)
+
+    # configure output
+    study_filepath = os.path.join(optuna_path, study_filename)
+    return input_data, study_filepath
+
+
+def run_optimisation(objective, study_filepath, verbose=False):
+    """Abstraction for Optuna fitting.
+
+    Includes early stopping logic and fixed seed for reproducibility.
+    The fitting can be either verbose or not but always dumps a
+    pickle file with the full logs of the process.
+
+    """
+    # early stopping logic (https://github.com/optuna/optuna/issues/1001)
+    class EarlyStoppingExceeded(optuna.exceptions.OptunaError):
+        early_stop = config.OPTUNA_EARLY_STOPPING
+        early_stop_count = 0
+        best_score = None
+
+    def early_stopping_opt(study, trial):
+        if EarlyStoppingExceeded.best_score is None:
+            EarlyStoppingExceeded.best_score = study.best_value
+
+        if study.best_value < EarlyStoppingExceeded.best_score:
+            EarlyStoppingExceeded.best_score = study.best_value
+            EarlyStoppingExceeded.early_stop_count = 0
+
+        else:
+            if (
+                EarlyStoppingExceeded.early_stop_count
+                > EarlyStoppingExceeded.early_stop
+            ):
+                EarlyStoppingExceeded.early_stop_count = 0
+                raise EarlyStoppingExceeded()
+            else:
+                EarlyStoppingExceeded.early_stop_count = (
+                    EarlyStoppingExceeded.early_stop_count + 1
+                )
+        return
+
+    # run the optimisation with sampler for reproducibility
+    if not verbose:
+        optuna.logging.set_verbosity(optuna.logging.WARNING)
+    sampler = TPESampler(seed=5)
+    study = optuna.create_study(sampler=sampler)
+
+    try:
+        study.optimize(objective, timeout=None, callbacks=[early_stopping_opt])
+    except EarlyStoppingExceeded:
+        print(
+            f"Early stopping exceeded: No new best scores \
+            after {config.OPTUNA_EARLY_STOPPING} iterations"
+        )
+    print(f"The best parameters were {study.best_params}")
+
+    # write the results out
+    with bz2.BZ2File(study_filepath, "w") as f:
+        cpickle.dump(study, f)
